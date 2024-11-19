d190fe447e9fd3ee66762f97d7e3e7242e23b2c8
Dariusz Okoński <darek200180@gmail.com>
2024-11-19 19:20:49 +0100

    add tests for playgroundpage

--- src/pages/PlaygroundPage.tsx
+++ src/pages/PlaygroundPage.tsx
@@ -1,7 +1,12 @@
-import TermsAndConditions from '../components/TermsAndConditions';
+import ExpandableText from '../components/ExpandableText';
+
+const longText =
+  'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia rem voluptatem ea impedit hic repudiandae. Dignissimos, incidunt sunt porro eligendi dolor aliquam harum suscipit dolorem veritatis, accusantium adipisci, aspernatur tenetur omnis fugiat facilis obcaecati harum facere odio debitis a illo sunt cum! Praesentium architecto quod voluptate.';
+
+// const shortText = 'Lorem ipsum dolor sit amet';
 
 const PlaygroundPage = () => {
-  return <TermsAndConditions />;
+  return <ExpandableText text={longText} />;
 };
 
 export default PlaygroundPage;
