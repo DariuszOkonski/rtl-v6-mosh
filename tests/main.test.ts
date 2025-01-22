4d1c4983f305895f74218290921f4d4ec49495f5
Dariusz Okoński <darek200180@gmail.com>
2025-01-22 19:45:09 +0100

    add basic test to call products

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -5,8 +5,13 @@ describe('group', () => {
   it('should', () => {
     expect(1).toBe(1);
   });
-  it('should call mock service worker', async () => {
+  it('should call mock service worker to categories endpoint', async () => {
     const response = await fetch('/categories');
+    const data = await response.json();
+    expect(data).toHaveLength(3);
+  });
+  it('should call mock service worker to products endpoint', async () => {
+    const response = await fetch('/products');
     const data = await response.json();
     expect(data).toHaveLength(3);
   });
