cabeb57f2b807bf3fdfcb9f79fdc0661e83c5c5f
Dariusz Okoński <darek200180@gmail.com>
2025-01-22 19:41:00 +0100

    add first test to fake server

--- tests/main.test.ts
+++ tests/main.test.ts
@@ -1,7 +1,13 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { it, expect, describe } from 'vitest';
 
 describe('group', () => {
   it('should', () => {
     expect(1).toBe(1);
+  });
+  it('should call mock service worker', async () => {
+    const response = await fetch('/categories');
+    const data = await response.json();
+    expect(data).toHaveLength(3);
   });
 });
